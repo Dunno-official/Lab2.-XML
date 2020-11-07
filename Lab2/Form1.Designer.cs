916b3aa930c88603ed631b3f37815808792366bb
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2020-11-07 23:22:24 +0200

    Initial commit

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.ToHTMLButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SaxRadioButton = new System.Windows.Forms.RadioButton();
+            this.DomRadioButton = new System.Windows.Forms.RadioButton();
+            this.LinqRadioButton = new System.Windows.Forms.RadioButton();
+            this.AuthorCheckBox = new System.Windows.Forms.CheckBox();
+            this.TitleCheckBox = new System.Windows.Forms.CheckBox();
+            this.GenreCheckBox = new System.Windows.Forms.CheckBox();
+            this.PriceCheckBox = new System.Windows.Forms.CheckBox();
+            this.PublishYearCheckBox = new System.Windows.Forms.CheckBox();
+            this.PublishYearComboBox = new System.Windows.Forms.ComboBox();
+            this.PriceComboBox = new System.Windows.Forms.ComboBox();
+            this.GenreComboBox = new System.Windows.Forms.ComboBox();
+            this.TitleComboBox = new System.Windows.Forms.ComboBox();
+            this.AuthorComboBox = new System.Windows.Forms.ComboBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.OpenXML = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(12, 411);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(141, 44);
+            this.ClearButton.TabIndex = 0;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // ToHTMLButton
+            // 
+            this.ToHTMLButton.Location = new System.Drawing.Point(306, 411);
+            this.ToHTMLButton.Name = "ToHTMLButton";
+            this.ToHTMLButton.Size = new System.Drawing.Size(141, 42);
+            this.ToHTMLButton.TabIndex = 1;
+            this.ToHTMLButton.Text = "Transform to HTML";
+            this.ToHTMLButton.UseVisualStyleBackColor = true;
+            this.ToHTMLButton.Click += new System.EventHandler(this.ToHTMLButton_Click);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(159, 411);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(141, 44);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // SaxRadioButton
+            // 
+            this.SaxRadioButton.AutoSize = true;
+            this.SaxRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SaxRadioButton.Location = new System.Drawing.Point(127, 49);
+            this.SaxRadioButton.Name = "SaxRadioButton";
+            this.SaxRadioButton.Size = new System.Drawing.Size(55, 21);
+            this.SaxRadioButton.TabIndex = 3;
+            this.SaxRadioButton.TabStop = true;
+            this.SaxRadioButton.Text = "Sax";
+            this.SaxRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // DomRadioButton
+            // 
+            this.DomRadioButton.AutoSize = true;
+            this.DomRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DomRadioButton.Location = new System.Drawing.Point(188, 49);
+            this.DomRadioButton.Name = "DomRadioButton";
+            this.DomRadioButton.Size = new System.Drawing.Size(61, 21);
+            this.DomRadioButton.TabIndex = 4;
+            this.DomRadioButton.TabStop = true;
+            this.DomRadioButton.Text = "Dom";
+            this.DomRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // LinqRadioButton
+            // 
+            this.LinqRadioButton.AutoSize = true;
+            this.LinqRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LinqRadioButton.Location = new System.Drawing.Point(255, 49);
+            this.LinqRadioButton.Name = "LinqRadioButton";
+            this.LinqRadioButton.Size = new System.Drawing.Size(60, 21);
+            this.LinqRadioButton.TabIndex = 5;
+            this.LinqRadioButton.TabStop = true;
+            this.LinqRadioButton.Text = "Linq";
+            this.LinqRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // AuthorCheckBox
+            // 
+            this.AuthorCheckBox.AutoSize = true;
+            this.AuthorCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AuthorCheckBox.Location = new System.Drawing.Point(62, 193);
+            this.AuthorCheckBox.Name = "AuthorCheckBox";
+            this.AuthorCheckBox.Size = new System.Drawing.Size(78, 21);
+            this.AuthorCheckBox.TabIndex = 6;
+            this.AuthorCheckBox.Text = "Author";
+            this.AuthorCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // TitleCheckBox
+            // 
+            this.TitleCheckBox.AutoSize = true;
+            this.TitleCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TitleCheckBox.Location = new System.Drawing.Point(62, 223);
+            this.TitleCheckBox.Name = "TitleCheckBox";
+            this.TitleCheckBox.Size = new System.Drawing.Size(62, 21);
+            this.TitleCheckBox.TabIndex = 7;
+            this.TitleCheckBox.Text = "Title";
+            this.TitleCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // GenreCheckBox
+            // 
+            this.GenreCheckBox.AutoSize = true;
+            this.GenreCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GenreCheckBox.Location = new System.Drawing.Point(62, 253);
+            this.GenreCheckBox.Name = "GenreCheckBox";
+            this.GenreCheckBox.Size = new System.Drawing.Size(75, 21);
+            this.GenreCheckBox.TabIndex = 8;
+            this.GenreCheckBox.Text = "Genre";
+            this.GenreCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // PriceCheckBox
+            // 
+            this.PriceCheckBox.AutoSize = true;
+            this.PriceCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PriceCheckBox.Location = new System.Drawing.Point(62, 283);
+            this.PriceCheckBox.Name = "PriceCheckBox";
+            this.PriceCheckBox.Size = new System.Drawing.Size(67, 21);
+            this.PriceCheckBox.TabIndex = 9;
+            this.PriceCheckBox.Text = "Price";
+            this.PriceCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // PublishYearCheckBox
+            // 
+            this.PublishYearCheckBox.AutoSize = true;
+            this.PublishYearCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PublishYearCheckBox.Location = new System.Drawing.Point(62, 314);
+            this.PublishYearCheckBox.Name = "PublishYearCheckBox";
+            this.PublishYearCheckBox.Size = new System.Drawing.Size(120, 21);
+            this.PublishYearCheckBox.TabIndex = 10;
+            this.PublishYearCheckBox.Text = "Publish year";
+            this.PublishYearCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // PublishYearComboBox
+            // 
+            this.PublishYearComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PublishYearComboBox.FormattingEnabled = true;
+            this.PublishYearComboBox.Location = new System.Drawing.Point(188, 311);
+            this.PublishYearComboBox.Name = "PublishYearComboBox";
+            this.PublishYearComboBox.Size = new System.Drawing.Size(154, 24);
+            this.PublishYearComboBox.TabIndex = 11;
+            // 
+            // PriceComboBox
+            // 
+            this.PriceComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PriceComboBox.FormattingEnabled = true;
+            this.PriceComboBox.Location = new System.Drawing.Point(188, 281);
+            this.PriceComboBox.Name = "PriceComboBox";
+            this.PriceComboBox.Size = new System.Drawing.Size(154, 24);
+            this.PriceComboBox.TabIndex = 12;
+            // 
+            // GenreComboBox
+            // 
+            this.GenreComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.GenreComboBox.FormattingEnabled = true;
+            this.GenreComboBox.Location = new System.Drawing.Point(188, 251);
+            this.GenreComboBox.Name = "GenreComboBox";
+            this.GenreComboBox.Size = new System.Drawing.Size(154, 24);
+            this.GenreComboBox.TabIndex = 13;
+            // 
+            // TitleComboBox
+            // 
+            this.TitleComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TitleComboBox.FormattingEnabled = true;
+            this.TitleComboBox.Location = new System.Drawing.Point(188, 221);
+            this.TitleComboBox.Name = "TitleComboBox";
+            this.TitleComboBox.Size = new System.Drawing.Size(154, 24);
+            this.TitleComboBox.TabIndex = 14;
+            // 
+            // AuthorComboBox
+            // 
+            this.AuthorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.AuthorComboBox.FormattingEnabled = true;
+            this.AuthorComboBox.Location = new System.Drawing.Point(188, 191);
+            this.AuthorComboBox.Name = "AuthorComboBox";
+            this.AuthorComboBox.Size = new System.Drawing.Size(154, 24);
+            this.AuthorComboBox.TabIndex = 15;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(471, 49);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(401, 362);
+            this.richTextBox.TabIndex = 16;
+            this.richTextBox.Text = "";
+            // 
+            // OpenXML
+            // 
+            this.OpenXML.Location = new System.Drawing.Point(130, 96);
+            this.OpenXML.Name = "OpenXML";
+            this.OpenXML.Size = new System.Drawing.Size(170, 46);
+            this.OpenXML.TabIndex = 17;
+            this.OpenXML.Text = "Open XML";
+            this.OpenXML.UseVisualStyleBackColor = true;
+            this.OpenXML.Click += new System.EventHandler(this.OpenXML_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(904, 484);
+            this.Controls.Add(this.OpenXML);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.AuthorComboBox);
+            this.Controls.Add(this.TitleComboBox);
+            this.Controls.Add(this.GenreComboBox);
+            this.Controls.Add(this.PriceComboBox);
+            this.Controls.Add(this.PublishYearComboBox);
+            this.Controls.Add(this.PublishYearCheckBox);
+            this.Controls.Add(this.PriceCheckBox);
+            this.Controls.Add(this.GenreCheckBox);
+            this.Controls.Add(this.TitleCheckBox);
+            this.Controls.Add(this.AuthorCheckBox);
+            this.Controls.Add(this.LinqRadioButton);
+            this.Controls.Add(this.DomRadioButton);
+            this.Controls.Add(this.SaxRadioButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.ToHTMLButton);
+            this.Controls.Add(this.ClearButton);
+            this.Name = "Form1";
+            this.Text = "ReadXml";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button ToHTMLButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.RadioButton SaxRadioButton;
+        private System.Windows.Forms.RadioButton DomRadioButton;
+        private System.Windows.Forms.RadioButton LinqRadioButton;
+        private System.Windows.Forms.CheckBox AuthorCheckBox;
+        private System.Windows.Forms.CheckBox TitleCheckBox;
+        private System.Windows.Forms.CheckBox GenreCheckBox;
+        private System.Windows.Forms.CheckBox PriceCheckBox;
+        private System.Windows.Forms.CheckBox PublishYearCheckBox;
+        private System.Windows.Forms.ComboBox PublishYearComboBox;
+        private System.Windows.Forms.ComboBox PriceComboBox;
+        private System.Windows.Forms.ComboBox GenreComboBox;
+        private System.Windows.Forms.ComboBox TitleComboBox;
+        private System.Windows.Forms.ComboBox AuthorComboBox;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button OpenXML;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        private List<RadioButton> _radioButtons;
+        private List<ComboBox> _comboBoxes;
+        private List<CheckBox> _checkBoxes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeLists();
+
+            SaxRadioButton.Checked = true; // по умолчанию
+        }
+
+        private void InitializeLists()
+        {
+            _radioButtons = new List<RadioButton>
+            { SaxRadioButton , DomRadioButton , LinqRadioButton};
+
+            _comboBoxes = new List<ComboBox>
+            { AuthorComboBox , TitleComboBox , GenreComboBox , PriceComboBox , PublishYearComboBox};
+
+            _checkBoxes = new List<CheckBox>
+            { AuthorCheckBox , TitleCheckBox , GenreCheckBox , PriceCheckBox , PublishYearCheckBox};
+        }
+
+        private void OpenXML_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "TXT File|*.xml";
+            openFileDialog.Title = "XML opening";
+            openFileDialog.RestoreDirectory = true;
+
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (Library.TryOpenXml(openFileDialog.FileName))
+                {
+                    Context context = new Context();
+                    int method = DefineMethod();
+                    ISearchStrategy concreteStrategy = Context.Strategies[method];
+
+                    context.SetStrategy(concreteStrategy);
+
+                    Library.Books = context.GetBooks();
+
+                    AddItemsToComboBox();
+                }
+            }
+        }
+        
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            richTextBox.Clear();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (CheckIfXmlOpened() && Check_CheckBoxes()) 
+            {
+                richTextBox.Clear();
+                Book criteria = GetCriteria();
+                
+                var matchesBooks = Library.SearchByCriteria(criteria);
+
+                ShowMatchBooks(matchesBooks);
+            }
+        }
+
+        private bool Check_CheckBoxes()
+        {
+            bool checkBoxSelected = false;
+
+            for (int i = 0; i < _checkBoxes.Count && !checkBoxSelected; ++i)
+                checkBoxSelected = _checkBoxes[i].Checked && _comboBoxes[i].SelectedItem != null;
+
+            if (!checkBoxSelected)
+                MessageBox.Show("Choose at least one criterion");
+
+            return checkBoxSelected;
+        }
+
+        private bool CheckIfXmlOpened()
+        {
+            bool XmlSelected= !(Library.Books.Count == 0);
+
+            if (!XmlSelected)
+                MessageBox.Show("Open your XML first");
+
+            return XmlSelected;
+        }
+
+        private int DefineMethod()
+        {
+            int answer;
+            int i;
+            bool buttonSelected = false;
+
+            for (i = 0; i < _radioButtons.Count && !buttonSelected; ++i) // если найдена кнопка true, цикл прервется
+                buttonSelected = _radioButtons[i].Checked;
+
+            answer = --i; // в цикле сначала происходит инкремент, потом проверка. Следовательно, отнимаем 1
+
+            return answer;
+        }
+
+        private Book GetCriteria()
+        {
+            Book answer = new Book();
+
+            string AddCriteria (ComboBox comboBox , CheckBox checkBox)
+            {
+                if (checkBox.Checked && comboBox.SelectedItem != null)
+                    return (string)comboBox.SelectedItem;
+                else
+                    return null;
+            }
+
+            answer.Author = AddCriteria(AuthorComboBox, AuthorCheckBox);
+            answer.Title = AddCriteria(TitleComboBox, TitleCheckBox);
+            answer.Genre = AddCriteria(GenreComboBox, GenreCheckBox);
+            answer.PublishYear = Convert.ToInt32(AddCriteria(PublishYearComboBox, PublishYearCheckBox));
+
+            if (PriceCheckBox.Checked)
+                Library.SetPriceRange((string)PriceComboBox.SelectedItem);
+            else
+                Library.PriceRangeFrom = -1;
+
+            return answer;
+        }
+
+        private void ShowMatchBooks(List<string> booksID)
+        {
+            foreach (var id in booksID)
+            {
+                richTextBox.Text += 
+
+                    id + '\n' + '\n' +
+                    Library.Books[id].Author + ". "+ Library.Books[id].Title + '\n' +
+                    Library.Books[id].Description + '\n';
+            }
+
+            if (booksID.Count == 0)
+                MessageBox.Show("No matching books :c");
+            
+        }
+
+        private void ToHTMLButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void AddItemsToComboBox()
+        {
+            void AddItem (ComboBox comboBox , string identifier)
+            {
+                if (!comboBox.Items.Contains(identifier))
+                    comboBox.Items.Add(identifier);
+            }
+
+            foreach (Book book in Library.Books.Values)
+            {
+                AddItem(AuthorComboBox, book.Author);
+                AddItem(TitleComboBox, book.Title);
+                AddItem(GenreComboBox, book.Genre);
+                AddItem(PublishYearComboBox, book.PublishYear.ToString());
+            }
+
+            SetPriceRange();
+        }
+
+        private void SetPriceRange()
+        {
+            PriceComboBox.Items.Add("0 - 10");
+            PriceComboBox.Items.Add("10 - 30");
+            PriceComboBox.Items.Add("30 - 50");
+        }
+    }
+}
+
+
+/*
+ * void ConvertControlToType <T> (List<Control> listToConvert , List<T> array) where T : Control
+            {
+                foreach (Control control in listToConvert)
+                {
+                    array.Add(control as T);
+                }
+            }
+            
+            List<Control> radioButtonsControl = Controls.OfType<RadioButton>().Cast<Control>().ToList(); // находим все Control типа RadioButton
+            List<Control> comboBoxControl = Controls.OfType<ComboBox>().Cast<Control>().ToList();
+            List<Control> checkBoxControl = Controls.OfType<CheckBox>().Cast<Control>().ToList();
+
+            ConvertControlToType(radioButtonsControl , _radioButtons); // конвертируем Contol в RadioButton
+            ConvertControlToType(comboBoxControl, _comboBoxes);
+            ConvertControlToType(checkBoxControl, _checkBoxes);
+
+            _radioButtons.Reverse(); // хз почему, но Controls.OfType находит их в обратном порядке
+            _comboBoxes.Reverse();
+            _checkBoxes.Reverse();
+ */
